f026096a9212e793be2e9eba2bea3273c595e6dd
tihmstar <tihmstar@gmail.com>
2015-12-14 20:06:45 +0100

    Initial Commit

---
+++ pzb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pzb
+//
+//  Created by tihmstar on 14.12.15.
+//  Copyright © 2015 tihmstar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
